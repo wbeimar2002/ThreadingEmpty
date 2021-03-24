4e403e6b7d49856758fee2c7fdb678728753bf38
Alexander Gonzalez <walex0402@hotmail.com>
2021-03-23 21:32:10 -0500

    Add project files.

---
+++ SprocketCache.Test/SprocketCacheTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SprocketCache.Test
+{
+    [TestFixture]
+    public class SprocketCacheTest
+    {
+        [Test]
+        public void CanGetSprocket()
+        {
+            var cache = new SprocketCache(new TestSprocketFactory());
+            var sprocket = cache.Get("test");
+            Assert.IsInstanceOf<Sprocket>(sprocket);
+        }
+
+        [Test]
+        public void SameKeyReturnsSameSprocket()
+        {
+            var cache = new SprocketCache(new TestSprocketFactory());
+            var sprocket1 = cache.Get("test");
+            var sprocket2 = cache.Get("test");
+            Assert.AreSame(sprocket1, sprocket2, "not the same");
+        }
+
+        [Test]
+        public void DifferentKeyReturnsDifferentSprocket()
+        {
+        }
+
+        [Test]
+        public void Expiry()
+        {
+        }
+
+        [Test]
+        public void IsThreadSafe()
+        {
+        }
+    }
+}
---
+++ SprocketCache/SprocketCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SprocketCache
+{
+    public class SprocketCache : ISprocketCache
+    {
+        private readonly ISprocketFactory sprocketFactory;
+
+        public SprocketCache(ISprocketFactory sprocketFactory)
+        {
+            this.sprocketFactory = sprocketFactory;
+        }
+        
+        public Sprocket Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
